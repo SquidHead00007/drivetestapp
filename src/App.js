9ece5bea94dc4fdf5ddf11682a6824488342f753
Sid <sid@live.com>
2025-02-20 20:24:14 -0800

    updated home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div id="appbody">
-      <h3>landing page - edits</h3>
+      <h3>landing page - edits 2 on 2/20</h3>
       <h4>summary</h4>
     </div>
   );
