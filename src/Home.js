407ebfc2990ad448d4e137734151982871c4c4c1
Sid <sid@live.com>
2025-02-22 15:57:08 -0800

    css for cards

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,25 @@
 import NavBar from "./NavBar";
 import React from 'react';
+import SectionList from "./data/doltestsection";
+import SectionCard from "./SectionCard";
 
 export function Home () {
+    const items = SectionList;
     return(
         <div id="BodyHome">
             <div>
                 <NavBar />
             </div>
+            
+            <div className="section-card">
+                {
+                    items.map((e) => {
+                        return (<SectionCard id={e.id} name={e.name} description={e.description} image={e.image} />)
+                    })
+                }
+            </div>
         </div>
+
     );
 };
 
--- src/data/doltestsection.js
+++ src/data/doltestsection.js
@@ -1,12 +1,12 @@
-const PerfumeData=[ 
-    { id:"general",         name:"General",                          image:require("../img/dolgeneral.jpg")},
-    { id:"parking",         name:"Parking",                          image:require("../img/parking.jpg")},
-    { id:"pavementmarking", name:"Pavement Markings",                image:require("../img/pavement.jpg")},
-    { id:"lanes",           name:"Reversible and Reserved Lanes",    image:require("../img/lanes.jpg")},
-    { id: "rightofway",     name:"Turning and Right-of-Way",         image:require("../img/rightofway.jpg")},
-    { id:"roundabouts",     name:"Roundabouts",                      image:require("../img/roundabouts.jpg")},
-    { id:"trafficsignal",   name:"Traffic Signals",                  image:require("../img/signal.jpg")},
-    { id:"trafficsign",     name:"Traffic Signs",                    image:require("../img/sign.jpg")},
-    { id: "expresstoll",name:"Two way turns and Express Tolls",      image:require("../img/express.jpg")},
+const SectionData=[ 
+    { id:"general",         name:"General",                          image:require("../img/dolgeneral.jpg"),  description:("awawawdawd")},
+    { id:"parking",         name:"Parking",                          image:require("../img/parking.jpg"),  description:("awawawdawd")},
+    { id:"pavementmarking", name:"Pavement Markings",                image:require("../img/pavement.jpg"),  description:("awawawdawd")},
+    { id:"lanes",           name:"Reversible and Reserved Lanes",    image:require("../img/lanes.jpg"),  description:("awawawdawd")},
+    { id: "rightofway",     name:"Turning and Right-of-Way",         image:require("../img/rightofway.jpg"),  description:("awawawdawd")},
+    { id:"roundabouts",     name:"Roundabouts",                      image:require("../img/roundabouts.jpg"),  description:("awawawdawd")},
+    { id:"trafficsignal",   name:"Traffic Signals",                  image:require("../img/signal.jpg"),  description:("awawawdawd")},
+    { id:"trafficsign",     name:"Traffic Signs",                    image:require("../img/sign.jpg"),  description:("awawawdawd")},
+    { id: "expresstoll",name:"Two way turns and Express Tolls",      image:require("../img/express.jpg"),  description:("awawawdawd")},
   ]
-  export default PerfumeData;
+  export default SectionData;
