f830d9bc91d3fd0dfb42a7c51fbf07a52a0fb2ef
Sid <sid@live.com>
2025-02-22 15:55:48 -0800

    section cards and description

---
+++ src/SectionCard.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+
+export function sectionCard (section) {
+    return (
+        <div className="card-item">
+            <div id={section.id} className="card-details">
+                <img src={section.image} alt={section.name} />
+                <h3>{section.name}</h3>
+                <p>{section.description} </p>
+            </div>
+        </div>
+    )
+}
+
+export default sectionCard;
