87b426d069a90b3d4b343c42a9644b404a647242
Sid <sid@live.com>
2025-02-22 15:08:31 -0800

    adding images and data

---
+++ src/data/doltestsection.js
@@ -0,0 +1,12 @@
+const PerfumeData=[ 
+    { id:"general",         name:"General",                          image:require("../img/dolgeneral.jpg")},
+    { id:"parking",         name:"Parking",                          image:require("../img/parking.jpg")},
+    { id:"pavementmarking", name:"Pavement Markings",                image:require("../img/pavement.jpg")},
+    { id:"lanes",           name:"Reversible and Reserved Lanes",    image:require("../img/lanes.jpg")},
+    { id: "rightofway",     name:"Turning and Right-of-Way",         image:require("../img/rightofway.jpg")},
+    { id:"roundabouts",     name:"Roundabouts",                      image:require("../img/roundabouts.jpg")},
+    { id:"trafficsignal",   name:"Traffic Signals",                  image:require("../img/signal.jpg")},
+    { id:"trafficsign",     name:"Traffic Signs",                    image:require("../img/sign.jpg")},
+    { id: "expresstoll",name:"Two way turns and Express Tolls",      image:require("../img/express.jpg")},
+  ]
+  export default PerfumeData;
