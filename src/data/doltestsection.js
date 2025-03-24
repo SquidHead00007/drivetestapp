f60f62f8118baa24ec48e70e93b1b151b0bacf07
Sid <sid@live.com>
2025-03-23 19:11:56 -0700

    section descriptions

--- src/data/doltestsection.js
+++ src/data/doltestsection.js
@@ -1,12 +1,12 @@
 const SectionData=[ 
-    { id:"general",         name:"General",                          image:require("../img/dolgeneral.jpg"),      description:("awawawdawd")},
+    { id:"general",         name:"General",                          image:require("../img/dolgeneral.jpg"),      description:("Questions regarding general section on practice test are listed here.")},
     { id:"parking",         name:"Parking",                          image:require("../img/parking.jpg"),         description:("Questions regarding parking section on practice test are listed here.")},
     { id:"pavementmarking", name:"Lane Markings",                    image:require("../img/pavement.jpg"),        description:("Questions regarding lane lines on practice test are listed here.")},
     { id:"lanes",           name:"Reversible and Reserved Lanes",    image:require("../img/lanes.jpg"),           description:("Questions regarding reserved lanes on practice test are listed here.")},
-    { id:"rightofway",      name:"Turning and Right-of-Way",         image:require("../img/rightofway.jpg"),      description:("awawawdawd")},
+    { id:"rightofway",      name:"Turning and Right-of-Way",         image:require("../img/rightofway.jpg"),      description:("Questions regarding right of way on practice test are listed here.")},
     { id:"roundabouts",     name:"Roundabouts",                      image:require("../img/roundabouts.jpg"),     description:("Questions regarding roundabouts on practice test are listed here.")},
     { id:"trafficsignal",   name:"Traffic Signals",                  image:require("../img/signal.jpg"),          description:("Questions regarding traffic signals on practice test are listed here.")},
     { id:"trafficsign",     name:"Traffic Signs",                    image:require("../img/sign.jpg"),            description:("Questions regarding traffic signs on practice test are listed here.")},
-    { id:"expresstoll",     name:"Two way turns and Express Tolls",  image:require("../img/express.jpg"),         description:("awawawdawd")},
+    { id:"expresstoll",     name:"Two way turns and Express Tolls",  image:require("../img/express.jpg"),         description:("Questions regarding two way turns and express tolls are listed here.")},
   ]
   export default SectionData;
